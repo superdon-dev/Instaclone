7129ce78c9621f62b2f45b7e166d467805e8845a
Superdon <logobysuperdon@gmail.com>
2020-06-20 16:43:14 +0200

    connected to db and added user model

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const { MONGO_URL } = require("./env");
+const app = express();
+const PORT = 3000;
+
+mongoose.connect(MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running...");
+});
---
+++ env.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGO_URL:
+    "mongodb+srv://superdon:@cluster0-qbpzh.mongodb.net/instaclone?retryWrites=true&w=majority",
+};

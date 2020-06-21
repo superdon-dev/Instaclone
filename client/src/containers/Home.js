c0b8afeaaf399b1e9ce33292ed7f03a00e343f0c
Superdon <logobysuperdon@gmail.com>
2020-06-21 15:15:06 +0200

    added home and profile pages

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div className="row">
+      <div className="card">
+        <div className="card-image">
+          <img
+            src="https://avatars3.githubusercontent.com/u/36360335?s=460&u=60efbfa1383ca82470a4333e8cfd2cd82dfb8ff3&v=4"
+            alt="Profile"
+          />
+        </div>
+        <div className="card-content">
+          <i class="material-icons">favorite</i>
+          <p>this is amazing post</p>
+          <input type="text" placeholder="Add a comment" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- client/src/containers/Profile.js
+++ client/src/containers/Profile.js
@@ -1,7 +1,63 @@
 import React from "react";
+import "./Profile.css";
 
 const Profile = () => {
-  return <h1>Profile</h1>;
+  return (
+    <React.Fragment>
+      <div className="row">
+        <div className="column">
+          <img
+            className="column-picture"
+            src="https://avatars3.githubusercontent.com/u/36360335?s=460&u=60efbfa1383ca82470a4333e8cfd2cd82dfb8ff3&v=4"
+            alt="Profile"
+          />
+        </div>
+        <div className="column">
+          <h4>Superdon</h4>
+          <div className="column-links">
+            <h6>40 posts</h6>
+            <h6>40 followers</h6>
+            <h6>40 following</h6>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="row">
+        <div className="gallery">
+          <img
+            className="gallery-item"
+            src="https://avatars3.githubusercontent.com/u/36360335?s=460&u=60efbfa1383ca82470a4333e8cfd2cd82dfb8ff3&v=4"
+            alt="Profile"
+          />
+          <img
+            className="gallery-item"
+            src="https://avatars3.githubusercontent.com/u/36360335?s=460&u=60efbfa1383ca82470a4333e8cfd2cd82dfb8ff3&v=4"
+            alt="Profile"
+          />
+          <img
+            className="gallery-item"
+            src="https://avatars3.githubusercontent.com/u/36360335?s=460&u=60efbfa1383ca82470a4333e8cfd2cd82dfb8ff3&v=4"
+            alt="Profile"
+          />
+          <img
+            className="gallery-item"
+            src="https://avatars3.githubusercontent.com/u/36360335?s=460&u=60efbfa1383ca82470a4333e8cfd2cd82dfb8ff3&v=4"
+            alt="Profile"
+          />
+          <img
+            className="gallery-item"
+            src="https://avatars3.githubusercontent.com/u/36360335?s=460&u=60efbfa1383ca82470a4333e8cfd2cd82dfb8ff3&v=4"
+            alt="Profile"
+          />
+          <img
+            className="gallery-item"
+            src="https://avatars3.githubusercontent.com/u/36360335?s=460&u=60efbfa1383ca82470a4333e8cfd2cd82dfb8ff3&v=4"
+            alt="Profile"
+          />
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Profile;
